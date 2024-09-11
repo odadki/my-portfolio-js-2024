0eff46f4513769fb036c41d5d4feb2c70233347f
odadki <93558662+odadki@users.noreply.github.com>
2024-09-10 18:25:47 -0700

    * Played around with some js on nav and section - currently commented out

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let h1 = document.getElementById("h1");
 
+// const aboutLink = document.getElementById("about-link");
+// const aboutSection = document.getElementById("about");
+// aboutLink.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   aboutSection.style.opacity = 1;
+// });
+
 // h1.innerHTML = "Changed";
