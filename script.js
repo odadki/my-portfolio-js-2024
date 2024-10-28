3e3c4805c66c9d79e3d4b4df206bcac08b779df3
odadki <93558662+odadki@users.noreply.github.com>
2024-10-27 23:45:21 -0700

    Updated main logo, attempted Intersection Observer for nav links - needs work

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ let h1 = document.getElementById("h1");
 // });
 
 // h1.innerHTML = "Changed";
+
+// Update style on nav link when it comes into view
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("nav-active");
+//     } else {
+//       entry.target.classList.remove("nav-active");
+//     }
+//   });
+// });
+
+// document.querySelectorAll(".nav-link").forEach((element) => {
+//   observer.observe(element);
+// });
