fd1917f8fbe8311fc05584b66426aab362d3ea5b
odadki <93558662+odadki@users.noreply.github.com>
2025-04-27 13:44:20 -0700

    mobile nav updates

--- script.js
+++ script.js
@@ -74,18 +74,23 @@ const mobileObserver = new IntersectionObserver(
         // Find the corresponding navigation link
         mobNavLinks.forEach((link) => {
           if (link.dataset.mobileNav === mobSectionId) {
-            link.classList.add("activeNav", "h2-animate");
+            link.classList.add("mob-active-nav");
           } else {
-            link.classList.remove("activeNav");
+            link.classList.remove("mob-active-nav");
           }
         });
 
+        entry.target.classList.add("h2-animate");
+
+        // Delay adding the animate-line class
+        setTimeout(() => {
+          entry.target.classList.add("animate-line");
+        }, 20);
+
         // entry.target.classList.add("h2-animate");
         //   observer.unobserve(entry.target);
-
       }
-    })
-
+    });
   },
   {
     rootMargin: "0px 0px -50% 0px",
@@ -116,17 +121,18 @@ window.addEventListener("resize", () => {
 });
 
 // Add mobile sticky nav after 100vh
-window.addEventListener('scroll', function() {
-  const nav = this.document.getElementById('mobile-sticky-nav');
-  const navCont2 = this.document.querySelector('.nav-cont-2');
+window.addEventListener("scroll", function () {
+  const nav = this.document.getElementById("mobile-sticky-nav");
+  const navCont2 = this.document.querySelector(".nav-cont-2");
   const scrollPosition = this.window.scrollY;
   const viewportHeight = this.window.innerHeight;
 
-  const navCont2Position = navCont2.getBoundingClientRect().bottom + this.window.scrollY;
+  const navCont2Position =
+    navCont2.getBoundingClientRect().bottom + this.window.scrollY;
 
   if (scrollPosition > navCont2Position) {
-    nav.classList.add('sticky');
+    nav.classList.add("sticky");
   } else {
-    nav.classList.remove('sticky');
+    nav.classList.remove("sticky");
   }
-})
+});
